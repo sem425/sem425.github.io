8b3c337bfd50b8786bd58473951502ac3974c251
Alexander Lebedev <lebastr@gmail.com>
2019-11-27 19:17:29 +0300

    hw2019_11_21: drawing machine

--- hw/nastya/2019_11_24/sketch.js
+++ hw/nastya/2019_11_24/sketch.js
@@ -1,5 +1,10 @@
+let T = 0.0;
+let Height = 800;
+let Width = 800;
+
 function setup() {
-  createCanvas(600, 600);
+    createCanvas(Width, Height);
+//    background(0);
 }
 
 function retP(x1, y1, r1, x2, y2, r2) {
@@ -32,27 +37,40 @@ function retP(x1, y1, r1, x2, y2, r2) {
 }
 
 function draw() {
-    background(0);
-    stroke(255);
-    fill(0,0,0,0);
+//    background(0);
 
-    let x1 = 300;
-    let y1 = 300;
-    let r1 = 100; 
+    translate(Width / 2, Height / 2);
+    scale(2);
+    //rotate(0.04*T);
+    
+    let x1 = -95 + 50*Math.cos(5*T);
+    let y1 = 50*Math.sin(5*T);
+    let r1 = 120; 
 
-    let x2 = mouseX;
-    let y2 = mouseY;
+    let x2 = 95 + 50*Math.cos(3.2*T);
+    let y2 = 50*Math.sin(3.2*T);
     let r2 = 150;
 
-    ellipse(x1, y1, 2*r1, 2*r1);
-    ellipse(x2, y2, 2*r2, 2*r2);
-
-    if(retP(x1, y1, r1, x2, y2, r2) != null) {
-	let [ix1, iy1, ix2, iy2] = retP(x1, y1, r1, x2, y2, r2);
-
+    //background(255);
+    stroke(0);
+    fill(0,0,0,0);
+    //line(-95, 0, x1, y1);
+    //line(95, 0, x2, y2);
+    //ellipse(x1, y1, 2*r1, 2*r1);
+    //ellipse(x2, y2, 2*r2, 2*r2);
+    
+    
+    let v = retP(x1, y1, r1, x2, y2, r2);
+    if(v != null) {
+	let [ix1, iy1, ix2, iy2] = v;
+	stroke(255, 0, 0);
 	fill(255, 0, 0);
-	ellipse(ix1, iy1, 10, 10);
+	ellipse(ix1, iy1, 2, 2);
 	fill(0, 0, 255);
-	ellipse(ix2, iy2, 10, 10);
+	stroke(0,0,255);
+	ellipse(ix2, iy2, 2, 2);
+	//line(x1, y1, ix1, iy1);
+	//line(x2, y2, ix1, iy1);
     }
+    T += 0.02;
 }
